806dec5ae900bd6a9022de4bb6d49f8f936eda12
FH300 <xhbl1220@gmail.com>
2023-12-19 11:03:59 +0100

    etchasketch.js

--- etchasketch.js
+++ etchasketch.js
@@ -1,10 +1,21 @@
-let gridDiv = document.querySelector('#grid');
+const gridDiv = document.querySelector('#grid');
+let square = document.createElement('div');
 
 
 for (let i = 1; i <= 256; i++) {
-  let square = document.createElement('div');
+  square = document.createElement('div');
   square.setAttribute('class', 'square');
-
   gridDiv.appendChild(square);
   square.textContent = '';
 }
+
+document.querySelectorAll('.square').forEach(square => 
+  square.addEventListener('mouseover', () => {
+    square.style.backgroundColor = 'blue';
+}));
+
+
+document.querySelectorAll('.square').forEach(square => 
+  square.addEventListener('mouseleave', () => {
+    square.style.backgroundColor = 'rgb(200, 200, 200)';
+}));
